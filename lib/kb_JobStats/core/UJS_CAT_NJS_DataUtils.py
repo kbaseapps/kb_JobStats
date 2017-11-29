bc7d61e4b03e1cac77ceb585e855047d2b6800b7
Qizhi Zhang <qzhang@kbasemacpro-qz.gateway.pace.com>
2017-11-28 20:19:04 -0600

    pass 'RCNES' to list_jobs2 to fetch all jobs

--- lib/kb_JobStats/core/UJS_CAT_NJS_DataUtils.py
+++ lib/kb_JobStats/core/UJS_CAT_NJS_DataUtils.py
@@ -582,7 +582,6 @@ class UJS_CAT_NJS_DataUtils:
 
 
     def init_clients(self, token):
-        '''
         #for prod environment
         self.ws_client = Workspace(self.workspace_url, token=token)
         self.cat_client = Catalog('https://kbase.us/services/catalog', auth_svc='https://kbase.us/services/auth/', token=token)
@@ -596,6 +595,7 @@ class UJS_CAT_NJS_DataUtils:
         self.njs_client = NarrativeJobService('https://ci.kbase.us/services/njs_wrapper', auth_svc='https://ci.kbase.us/services/auth/', token=token)
         self.ujs_client = UserAndJobState('https://ci.kbase.us/services/userandjobstate', auth_svc='https://ci.kbase.us/services/auth/', token=token)
         self.uprf_client = UserProfile('https://ci.kbase.us/services/user_profile/rpc', auth_svc='https://ci.kbase.us/services/auth/', token=token)
+        '''
 
 
     def process_user_parameters(self, params):
